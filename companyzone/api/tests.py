5c6e23932b5cc2e6d7e7ed50e45c2d5085863aad
manoj <manojabhisargahas1@gmail.com>
2026-08-28 12:32:07 +0530

    Imported dynamic User in API Test Cases

--- companyzone/api/tests.py
+++ companyzone/api/tests.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model # import the dynamic user
+
+User = get_user_model()
 
 # Create your tests here.
 class CompanyZoneAPITests(APITestCase):
